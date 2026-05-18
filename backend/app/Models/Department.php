327247a80a109c383682d75f8375df1d2c6cbbac
Developpeur Stagaire <dev@stagaire.com>
2026-05-18 14:44:12 +0100

    feat: implementation du modele Department

---
+++ backend/app/Models/Department.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Department extends Model
+{
+    protected $fillable = ['name', 'description'];
+
+    public function users(): HasMany
+    {
+        return $this->hasMany(User::class);
+    }
+
+    public function interns(): HasMany
+    {
+        return $this->hasMany(Intern::class);
+    }
+
+    public function internshipApplications(): HasMany
+    {
+        return $this->hasMany(InternshipApplication::class);
+    }
+}
+
+
