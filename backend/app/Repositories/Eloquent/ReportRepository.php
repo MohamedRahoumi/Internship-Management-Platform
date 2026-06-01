f106b76ed832d89b12ef68e76e64826811fa260c
Developpeur Stagaire <dev@stagaire.com>
2026-06-01 08:50:31 +0100

    feat: creation du repository ReportRepository

---
+++ backend/app/Repositories/Eloquent/ReportRepository.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Repositories\Eloquent;
+
+use App\Models\InternshipReport;
+use App\Repositories\Interfaces\ReportRepositoryInterface;
+
+class ReportRepository implements ReportRepositoryInterface
+{
+    public function all(array $filters = [])
+    {
+        $query = InternshipReport::with('intern.user');
+        if (!empty($filters['status'])) {
+            $query->where('status', $filters['status']);
+        }
+        if (!empty($filters['intern_id'])) {
+            $query->where('intern_id', $filters['intern_id']);
+        }
+        return $query->latest()->get();
+    }
+
+    public function findById(int $id): ?InternshipReport
+    {
+        return InternshipReport::with('intern.user', 'intern.department')->find($id);
+    }
+
+    public function findByIntern(int $internId): ?InternshipReport
+    {
+        return InternshipReport::where('intern_id', $internId)->first();
+    }
+
+    public function create(array $data): InternshipReport
+    {
+        return InternshipReport::create($data);
+    }
+
+    public function update(InternshipReport $report, array $data): InternshipReport
+    {
+        $report->update($data);
+        return $report->fresh();
+    }
+
+    public function delete(InternshipReport $report): bool
+    {
+        return $report->delete();
+    }
+}
+
+
