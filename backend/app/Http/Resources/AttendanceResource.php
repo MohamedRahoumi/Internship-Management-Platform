6a1a53fe4565fa1872bd9e2f23e3efaaf9b8c8e9
Developpeur Stagaire <dev@stagaire.com>
2026-06-13 21:28:25 +0100

    fix: resolution du bug de pagination dans la liste des stagiaires

---
+++ backend/app/Http/Resources/AttendanceResource.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class AttendanceResource extends JsonResource
+{
+    public function toArray($request): array
+    {
+        return [
+            'id' => $this->id,
+            'intern_id' => $this->intern_id,
+            'scanner' => new UserResource($this->whenLoaded('scanner')),
+            'check_in_at' => $this->check_in_at,
+            'check_out_at' => $this->check_out_at,
+            'date' => $this->date,
+            'status' => $this->status,
+            'created_at' => $this->created_at,
+        ];
+    }
+}
