7b14ba45e6a386ee51371001e734bfffaa6de730
Developpeur Stagaire <dev@stagaire.com>
2026-06-21 06:18:56 +0100

    chore: configuration du service mailhog pour les emails

---
+++ backend/app/Repositories/Interfaces/ReportRepositoryInterface.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Repositories\Interfaces;
+
+use App\Models\InternshipReport;
+
+interface ReportRepositoryInterface
+{
+    public function all(array $filters = []);
+    public function findById(int $id): ?InternshipReport;
+    public function findByIntern(int $internId): ?InternshipReport;
+    public function create(array $data): InternshipReport;
+    public function update(InternshipReport $report, array $data): InternshipReport;
+    public function delete(InternshipReport $report): bool;
+}
